75f3e31daa21fe18a62b1cf8c05a9be46d8c4c48
ronikdhaduk <ronikdhaduk8@gmail.com>
2024-07-09 11:38:43 +0530

    list and dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,11 @@
+dict = {
+    'name' : 'Ronik',
+    'surname' : 'dhaduk'
+}
+print(dict)
+print(dict['name'])
+for i in dict:
+    print(dict[i])
+
+dict['city'] = "Rajkot"
+print(dict)
---
+++ list.py
@@ -0,0 +1,12 @@
+# 13. Create a python program to print list of element
+List = ['Ram', 'Ronik', 'Meet', 'Raj']
+
+print(List)
+print(List[1])
+
+for i in List:
+    print(i)
+    
+# update element in list and print uppdate list
+List[2] = "Prins"
+print(List)
