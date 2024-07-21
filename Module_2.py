7d0a784ef241486ef5f7fc1af54fdf7b3984cd7c
ronikdhaduk <ronikdhaduk8@gmail.com>
2024-07-21 15:10:15 +0530

    22 Modele Program

---
+++ Module_2.py
@@ -0,0 +1,8 @@
+import Modules.Module_2_22 as f
+
+name = f.dict1['name']
+age = f.dict1['age']
+hobby = f.dict1['hobby']
+eduction = f.dict1['Eduction']
+
+print("Name:", name, "\nAge:", age, "\nHobby:", hobby, "\nEduction:", eduction)
---
+++ Modules/Module_2_22.py
@@ -0,0 +1,6 @@
+dict1 = {
+    "name":"Virat kohali",
+    "age" : 39,
+    "hobby" : "Cricket",
+    "Eduction" : "Diploma Eng. at mumbai university"
+}
